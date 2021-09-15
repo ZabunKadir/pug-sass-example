8e4b81c409f3c95525f662ca1580debf6378f9e0
ZabunKadir <kadir.315@hotmail.com>
2021-09-16 01:17:42 +0300

    First Commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(".Header-switch").on("click", function () {
+  if ($("Header").is(".menu-show")) {
+    $("Header").removeClass("menu-show");
+  } else {
+    $("Header").addClass("menu-show");
+  }
+});
+
+$(".Highlights-slider").flickity({
+  // options
+  cellAlign: "left",
+  contain: true,
+  prevNextButtons: false,
+  pageDots: false,
+});
